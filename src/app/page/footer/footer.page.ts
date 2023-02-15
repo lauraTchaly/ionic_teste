5110fc8211fdb1b5596ad08952f52d9371df07d0
lauraTchaly <lauramanes@outlook.com>
2023-02-15 16:50:49 -0300

    MODIFICANDO FOOTER

--- src/app/page/footer/footer.page.ts
+++ src/app/page/footer/footer.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DatabaseService } from 'src/app/dataBase/database.service';
+import { UtilityService } from 'src/app/dataBase/Utility.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private database: DatabaseService,
+    private alertctrl: AlertController,
 
-  ngOnInit() {
+    // Serviços de interação com o usuário
+    private utilidades: UtilityService
+  ) { }
+
+  ngOnInit() {}
+
+  // Método do alertando
+  async alertando(){
+
+    const alert = this.alertctrl.create({
+
+      mode: "ios",
+      header:"Cadastro de Produto",
+      inputs:[
+        {
+          name:"produto",
+          type:"text",
+          placeholder:"Informe o produto"
+        },
+        {
+          name:"cat",
+          type:"text",
+          placeholder:"Informe a Categoria"
+        },
+        {
+          name:"qtr",
+          type:"number",
+          placeholder:"Informe a Quantidade"
+        }
+      ]
+    })
   }
 
 }
