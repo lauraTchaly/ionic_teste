339565b5ac63fee67185f12016fb789ecc6e0a41
lauraTchaly <lauramanes@outlook.com>
2023-03-03 15:02:45 -0300

    CONFIGURANDO HOME PARA O BANCO

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActionSheetController, AlertController } from '@ionic/angular';
+import { Produto } from '../model/produto';
+import { DatabaseService } from '../dataBase/database.service';
+import { UtilityService } from '../dataBase/Utility.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  constructor(
+
+    // NOSSO OBJETO PARA OS SERVIÇOS DO BANCO DE DADOS 
+  private DataBase: DatabaseService,
+
+  // OBJETO PARA MANIPULAR OS MÉTODOS CLASSE IONIC
+  private actionSheet: ActionSheetController,
+
+  // OBJETO PARA MANIPULAR OS SERVIÇOS DA CLASSE SERVIÇOS
+  private utilidade: UtilityService
+
+  ) {}
+
+  
+  listaProdutos: Produto[] = [];
+
+  // CARREGAR O MÉTODO DO INÍCIO DA PÁGINA
+
+  ngOnInit(): {
+    
+  }
+
+
 
 
 }
